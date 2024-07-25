5bf31b36d32c951ab72421f69b40993a0fbe8e62
TayluanSantos <tayluanjesus@gmail.com>
2024-07-25 12:00:09 -0300

    Criando a entidade Categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Categoria } from './categoria/entities/categoria.entity';
+import { CategoriaModule } from './categoria/categoria.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_projeto_integrador',
+      entities: [Categoria],
+      synchronize: true,
+      logging: true 
+    }),
+    CategoriaModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
