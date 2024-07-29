a9f0df7279d9fb108dd1d172dfaa602c5b63bc79
Pamela <contatopamela@outlook.com>
2024-07-29 14:58:52 -0300

    Finalizando CRUD-Controller

--- src/categoria/categoria.module.ts
+++ src/categoria/categoria.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { Categoria } from "./entities/categoria.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { CategoriaService } from "./services/categoria.service";
+import { CategoriaController } from "./controller/categoria.controller";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Categoria])],
-    controllers: [],
+    controllers: [CategoriaController],
     providers: [CategoriaService],
     exports:[TypeOrmModule]
   })
