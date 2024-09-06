08582f3b4df3a5025b8970cc6370472d1b46256d
Larissa <larissaangioni@gmail.com>
2024-09-06 09:36:49 -0300

    fix: Auth Service

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -41,6 +41,7 @@ export class AuthService{
             nome: buscaUsuario.nome,
             usuario: usuarioLogin.usuario,
             foto: buscaUsuario.foto,
+            tipo: buscaUsuario.tipo,
             token: `Bearer ${this.jwtService.sign(payload)}`,
         }
 
